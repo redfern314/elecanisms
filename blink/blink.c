31fbec9f641e748c7e0f787443bcfc5750f33af0
Derek Redfern <redfern.derek@gmail.com>
2015-01-22 12:25:36 -0500

    Speed and LED color control

--- blink/blink.c
+++ blink/blink.c
@@ -9,17 +9,62 @@ int16_t main(void) {
     init_ui();
     init_timer();
 
+    int sw1Mem=0;
+    int sw2Mem=0;
+    int sw3Mem=0;
+    float LEDtime = 1;
+
+    int selectedLED = 0;
+    _LED LEDlist[] = {led1,led2,led3};
+
     led_on(&led1);
-    timer_setPeriod(&timer2, 0.5);
+    timer_setPeriod(&timer2, LEDtime);
     timer_start(&timer2);
 
     while (1) {
         if (timer_flag(&timer2)) {
             timer_lower(&timer2);
-            led_toggle(&led1);
+            led_toggle(&LEDlist[selectedLED]);
         }
-        led_write(&led2, !sw_read(&sw2));
-        led_write(&led3, !sw_read(&sw3));
+
+        if (sw_read(&sw1) && !sw1Mem) {
+            selectedLED++;
+            selectedLED %= 3;
+            sw1Mem = 1;
+
+            int i;
+            for(i=0;i<3;i++) {
+                led_off(&LEDlist[i]);
+            }
+        }
+
+        if(!sw_read(&sw1)) {
+            sw1Mem=0;
+        }
+
+        if (sw_read(&sw2) && !sw2Mem) {
+            led_write(&led2, !sw_read(&sw2));
+            sw2Mem=1;
+            LEDtime*=2;
+            timer_setPeriod(&timer2, LEDtime);
+            timer_start(&timer2);
+        }
+
+        if(!sw_read(&sw2)) {
+            sw2Mem=0;
+        }
+
+        if (sw_read(&sw3) && !sw3Mem) {
+            led_write(&led3, !sw_read(&sw3));
+            sw3Mem=1;
+            LEDtime/=2;
+            timer_setPeriod(&timer2, LEDtime);
+            timer_start(&timer2);
+        }
+
+
+        if(!sw_read(&sw3)) {
+            sw3Mem=0;
+        }
     }
 }
-
